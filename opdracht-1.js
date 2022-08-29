e91bb1bc19ef8c57f38f32be7d255bb5d16a8f53
linsiana <joeriluteyn@gmail.com>
2022-08-29 16:32:58 +0200

    first assigment done

--- opdracht-1.js
+++ opdracht-1.js
@@ -1,4 +1,5 @@
 // VOORRAAD ARRAY MET TV'S
+
 const inventory = [
     {
         type: '43PUS6504/12',
@@ -165,7 +166,34 @@ const inventory = [
 
 // Opdracht 1 - Array Methoden
 // Opdracht 1a: Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de console.
-//     Opdracht 1b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn. Log de uitkomst in de console.
+// map methode op names
+
+const tvNames = inventory.map((tvName) => {
+    return tvName.name
+})
+
+// console.log(tvNames)
+
+// Opdracht 1b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn. Log de uitkomst in de console.
+// filter methode waar origalStock === sold
+
+const tvsSoldOut = inventory.filter((tvSoldOut) => {
+    if (tvSoldOut.originalStock === tvSoldOut.sold) {
+        return true
+    }
+})
+
+// console.log(tvsSoldOut)
+
 // Opdracht 1c: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken. Log de uitkomst in de console.
+// filter methode op ambilight === true
+const tvsWithAmbilight = inventory.filter((tvWithAmbilight) => {
+    return tvWithAmbilight.options.ambiLight === true
+})
+
+// console.log(tvsWithAmbilight)
+
 // Opdracht 1d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de console.
 
+inventory.sort((a, b) => a.price - b.price)
+console.log(inventory)
